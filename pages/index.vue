391e232e14117257a7a0a876f387ba848ed187b6
Alexis Germain <50668811+Mazzarno@users.noreply.github.com>
2024-05-30 12:22:34 +0200

    '1.1.4'

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,5 @@
 <template>
-  <div
-    id="container"
-    class="bg-black w-screen h-screen -z-40 snap-y snap-proximity"
-  >
+  <div class="bg-black w-screen h-screen -z-40 snap-y snap-proximity container">
     <section
       class="relative flex items-center justify-center h-screen overflow-hidden w-screen snap-center section"
       v-for="(work, index) in works.slice(0, 20)"
@@ -21,7 +18,7 @@
               :data-text="'&nbsp-&nbsp' + work.name"
               id="name"
             >
-              <span id="name">{{ "&nbsp-&nbsp" + work.name }}</span>
+              <span id="name">{{ '&nbsp-&nbsp' + work.name }}</span>
             </h1>
           </div>
         </NuxtLink>
@@ -46,30 +43,31 @@
 </template>
 
 <script setup lang="ts">
-import { useProjectsStore } from "@/store/projects";
-const projectsStore = useProjectsStore();
-const works = projectsStore.projects.works.data;
-const { $gsap, $ScrollTrigger, $ScrollTo } = useNuxtApp();
-onMounted(() => {
-  $gsap.to("#section-0 h1", {
-    scrollTrigger: ".section",
-    toggleActions: "restart complete restart reset",
-    duration: 10,
-    rotation: 360,
-  });
+import { useProjectsStore } from '@/store/projects'
+const projectsStore = useProjectsStore()
+const works = projectsStore.projects.works.data
+const { $gsap, $ScrollTrigger, $ScrollTo } = useNuxtApp()
 
-  const videos = document.querySelectorAll(".video");
+onMounted(() => {
+
+  $gsap.to('#section-1 h1', {
+    scrollTrigger: '#section-1',
+    duration: 1,
+    rotation: 360,
+  })
+
+  const videos = document.querySelectorAll('.video')
   const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
-        entry.target.play();
+        entry.target.play()
       } else {
-        entry.target.pause();
+        entry.target.pause()
       }
-    });
-  });
-  videos.forEach((video) => observer.observe(video));
-});
+    })
+  })
+  videos.forEach((video) => observer.observe(video))
+})
 </script>
 
 <style scoped>
